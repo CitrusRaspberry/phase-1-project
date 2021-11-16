f48e2a82185711a04371f962aa01704750e89abb
Joshua Holmes <joshua.phillip.holmes@gmail.com>
2021-11-16 12:10:41 -0600

    Added js so game content loads with info from dictionary. Changed button colors slightly

--- scripts.js
+++ scripts.js
@@ -5,6 +5,12 @@ const introButtons = introContent.querySelectorAll("button");
 const content = document.querySelector("#content");
 const letterBtns = content.querySelectorAll("button.letter");
 const urlDictionary = "https://api.dictionaryapi.dev/api/v2/entries/en/";
+const wordContainer = content.querySelector("#word-container");
+const lettersLeftTxt = content.querySelector("#letters-left");
+const originHdr = content.querySelector("#origin-header");
+const originTxt = content.querySelector("#origin-text");
+const defTxt = content.querySelector("#definition-text");
+let savedEvent;
 
 /////////////////////////////////////////////
 //// INTRO STYILING
@@ -27,24 +33,19 @@ function introMoveUp() {
   setTimeout( () => {
     introContainer.style.animationDuration = `${introMoveUpDuration}s`;
     introContainer.style.animationName = "introMoveUp";
-    introContent.className += " fade-in";
+    introContent.classList.add("fade-in");
   }, introMoveUpDelay * 1000)
 }
 
 /////////////////////////////////////////////////////
 //// GAME LOGIC
 
-function dictionarySearchFor(word) {
-  fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${word}`)
-  .then(r => r.json())
-  .then(data => console.log(data[0]))
-  .catch(error => console.log("RUH ROH!! -->", error));
-}
 function initMenu() {
   introMoveUp();
   introButtons.forEach(button => button.addEventListener("click", initGame));
 }
 function initGame(e) {
+  savedEvent = e;
   introAndContentSwap(e);
   let urlRandomWord = "https://random-word-form.herokuapp.com/random/";
   switch (e.target.name) {
@@ -67,20 +68,45 @@ function initGame(e) {
     default:
       console.error("MAYDAY!! URL CAN'T BE FOUND! WE'RE GOING DOWN!! *crash*");
   }
-  renderGame(urlRandomWord);
+  getRandomWord(urlRandomWord);
 }
-function renderGame(urlRandomWord) {
-
+function renderGame(wordObj) {
+  console.log("YAY", wordObj);
+  defTxt.textContent = wordObj.meanings[0].definitions[0].definition;
+  if (wordObj.origin) {
+    originTxt.textContent = wordObj.origin;
+    originTxt.style.display = "";
+    originHdr.style.display = "";
+  }
+  lettersLeftTxt.textContent = `${wordObj.word.length} letters left`;
+  wordContainer.replaceChildren();
+  for (letter of wordObj.word) {
+    const newLetter = document.createElement("div")
+    newLetter.className = "word-letter";
+    wordContainer.append(newLetter);
+  }
+  startGame();
 }
 function startGame() {
-
+  // letterBtns.forEach(btn => btn.addEventListener("click", ));
 }
-function fetchGET(url, cb, errorMsg = "Fetch error -->") {
-  // const config = { method: "GET", headers: { "Content-Type": "application/json", Accept: "application/json" } }
-  fetch(url, config)
+function fetchGET(url, cb, randomWordIfError) {
+  fetch(url)
   .then(r => r.json())
   .then(data => cb(data))
-  .catch(error => console.error(errorMsg, error))
+  .catch(error => {
+    console.error("HOLD THE PHONE!!! Fetch error -->", error);
+    randomWordIfError ? getRandomWord(url) : console.error("randomWordIfError = false");
+  });
+}
+function getRandomWord(urlRandomWord) {
+  fetchGET(urlRandomWord, data => dictionarySearchFor(data[0], urlRandomWord), true);
+}
+function dictionarySearchFor(word, urlRandomWord) {
+  fetchGET(urlDictionary + word, data => {
+    const isSuccess = !!data[0];
+    isSuccess ? renderGame(data[0]) : getRandomWord(urlRandomWord);
+  });
 }
 
 
@@ -89,4 +115,3 @@ function fetchGET(url, cb, errorMsg = "Fetch error -->") {
 //// GAME BEGINS
 
 initMenu();
-// dictionarySearchFor("jfalskdfj;as")
